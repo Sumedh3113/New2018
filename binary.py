ca7ab7daecf0368272ecb743d3f1411e394feb2d
sumedh3113 <sumedhdeshpande31@gmail.com>
2018-07-01 22:21:30 -0700

    Convert int to binary , reverse and then convert to int

---
+++ binary.py
@@ -0,0 +1,19 @@
+"""
+return reverse of binary
+and then convert the reverse value to integer
+"""
+import sys
+
+def rev_bin(num):
+     x = "{0:b}".format(num)
+     return list(reversed(x))
+     
+value = int(sys.stdin.readline())     
+     
+result = rev_bin(value)
+
+y = ''.join(result)
+
+z = int(y,2)
+
+sys.stdout.write(str(z))
