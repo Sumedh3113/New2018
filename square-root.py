301d171eba2ee41a1b50cc53085fe55a23bdf9e9
sumedh3113 <sumedhdeshpande31@gmail.com>
2018-07-10 19:42:08 -0700

    square root of integer

---
+++ square-root.py
@@ -0,0 +1,14 @@
+"""
+Return square root of integer
+"""
+class Solution:
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(x**0.5)
+		
+		
+x = Solution()
+print(x.Solution())
